40cffe7d016b63c113088838362093525160743f
Michiel Van den Berghe <michiel.vdb@gmail.com>
2025-07-05 17:02:01 +0200

    wip

--- nanoleaf2mqtt/run.sh
+++ nanoleaf2mqtt/run.sh
@@ -5,4 +5,5 @@ MQTT_HOST=$(bashio::services mqtt "host")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 DATABASE_PATH="/data/nanoleaf2mqtt.db"
+bashio::log.info "Starting Nanoleaf2MQTT with MQTT host: $MQTT_HOST, user: $MQTT_USER, database path: $DATABASE_PATH"
 /app/.venv/bin/python -m nanoleaf2mqtt.main 
