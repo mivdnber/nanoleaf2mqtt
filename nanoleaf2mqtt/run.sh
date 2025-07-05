20cf488d186dc9a8b8d6875da61f129adfbe5534
Michiel Van den Berghe <michiel.vdb@gmail.com>
2025-07-05 17:19:25 +0200

    wip

--- nanoleaf2mqtt/run.sh
+++ nanoleaf2mqtt/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/with-contenv bashio
 
 cd /app
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
-MQTT_PORT=$(bashio::services mqtt "port")
+export MQTT_HOST=$(bashio::services mqtt "host")
+export MQTT_USER=$(bashio::services mqtt "username")
+export MQTT_PASSWORD=$(bashio::services mqtt "password")
+export MQTT_PORT=$(bashio::services mqtt "port")
 
 DATABASE_PATH="/data/nanoleaf2mqtt.db"
 bashio::log.info "Starting Nanoleaf2MQTT with MQTT host: $MQTT_HOST, port: $MQTT_PORT, user: $MQTT_USER, database path: $DATABASE_PATH"
