b3ea36b6973a1246101bd8f8ddc8494b285dbbbe
Michiel Van den Berghe <michiel.vdb@gmail.com>
2025-07-05 17:17:14 +0200

    wip

--- nanoleaf2mqtt/nanoleaf2mqtt/main.py
+++ nanoleaf2mqtt/nanoleaf2mqtt/main.py
@@ -31,40 +31,6 @@ from nanoleaf2mqtt.nanoleaf import (
     NanoleafError,
 )
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "handlers": {
-        "default": {
-            "class": "logging.StreamHandler",
-            "formatter": "nanoleaf2mqtt",
-            "stream": "ext://sys.stderr",
-            "level": LOG_LEVEL,
-        }
-    },
-    "formatters": {
-        "nanoleaf2mqtt": {
-            "format": "[%(asctime)s] %(levelname)s %(name)s - %(message)s",
-            # "datefmt": "%Y-%m-%d %H:%M:%S",
-        }
-    },
-    "loggers": {
-        "httpx": {
-            "handlers": ["default"],
-            "level": "CRITICAL",
-        },
-        "httpcore": {
-            "handlers": ["default"],
-            "level": "CRITICAL",
-        },
-    },
-    "root": {
-        "handlers": ["default"],
-        "level": LOG_LEVEL,
-    },
-}
-
-logging.config.dictConfig(LOGGING_CONFIG)
-
 logger = logging.getLogger(__name__)
 
 
